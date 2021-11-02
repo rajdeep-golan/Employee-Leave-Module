1081471bde857142db77d22b0b62da7db4013099
RAJDEEP SINGH GOLAN <38165635+rajdeep-golan@users.noreply.github.com>
2021-11-02 17:54:57 +0530

    Add files via upload

---
+++ heads/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				Golan Leave System <a href="https://aci.edu.gh/" target="_blank"><span>developed by </span> Rajdeep Singh Golan</a>
+			</div>
---
+++ heads/includes/right_sidebar.php
@@ -0,0 +1,17 @@
+<div class="right-sidebar">
+		<div class="sidebar-title">
+			<h3 class="weight-600 font-16 text-blue">
+				Layout Settings
+				<span class="btn-block font-weight-400 font-12">User Interface Settings</span>
+			</h3>
+			<div class="close-sidebar" data-toggle="right-sidebar-close">
+				<i class="icon-copy ion-close-round"></i>
+			</div>
+		</div>
+		
+
+				<div class="reset-options pt-30 text-center">
+					<button class="btn btn-danger" id="reset-settings">Reset Settings</button>
+				</div>
+			
+	</div>
